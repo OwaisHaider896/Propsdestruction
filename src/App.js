8ca275851fc650c94108b833ab7a4844baba4dee
Owais Naqvi <65233517+OwaisHaider896@users.noreply.github.com>
2021-01-04 16:13:32 +0500

    Make List separately

--- src/App.js
+++ src/App.js
@@ -1,58 +1,7 @@
 import React from "react";
 import "./styles.css";
 import Employee from "./Employee";
-
-const employeeList = [
-  {
-    name: "Owais",
-    age: "21",
-    salary: "40000",
-    image:
-      "https://lh3.googleusercontent.com/ogw/ADGmqu8ioXlESvm9_UjXgWDRUnha9RLTwpAp8Pxr5ZBW=s83-c-mo"
-  },
-  {
-    name: "Amir",
-    age: "20",
-    salary: "80000",
-    image:
-      "https://avatars2.githubusercontent.com/u/43801058?s=400&u=dd43998f131ecef8098b0fd6584a53e50a2ada29&v=4"
-  },
-  {
-    name: "Osama",
-    age: "21",
-    salary: "50000",
-    image:
-      "https://avatars0.githubusercontent.com/u/62401172?s=400&u=1ab8c8579626e9a599599116175d00951085db77&v=4"
-  },
-  {
-    name: "Usman",
-    age: "21",
-    salary: "60000",
-    image:
-      "https://avatars0.githubusercontent.com/u/49396362?s=400&u=8876aa1210515ace059bba8d06086879f492e962&v=4"
-  },
-  {
-    name: "Ali",
-    age: "22",
-    salary: "30000",
-    image:
-      "https://avatars0.githubusercontent.com/u/18057994?s=400&u=08073e19e05cc0e7a60a27c3738f90578bd80f88&v=4"
-  },
-  {
-    name: "Humayun",
-    age: "23",
-    salary: "10000",
-    image:
-      "https://avatars2.githubusercontent.com/u/53262712?s=400&u=4ddcdf4e7bb7561ad87c2af790fffd7739bdbd6e&v=4"
-  },
-  {
-    name: "Omaima",
-    age: "21",
-    salary: "30000",
-    image:
-      "https://avatars2.githubusercontent.com/u/37713288?s=400&u=1f9c55a08a77bb414b43a923e0d2be2d15b17549&v=4"
-  }
-];
+import employeeList from "./List";
 
 export default function App() {
   return (
